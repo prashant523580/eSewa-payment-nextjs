25d999be6b967412f349e96aceec493cb42d998c
prashant523580 <prashant.thapa62758@gmail.com>
2025-07-21 07:06:00 +0545

    removed unusual

--- src/app/api/payment/failure/route.ts
+++
@@ -1,7 +0,0 @@
-// app/api/payment/failure/route.ts
-
-import {  NextResponse } from "next/server";
-
-export async function POST() {
-    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/failure`);
-  }
